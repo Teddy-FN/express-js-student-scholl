e443979e58992fd88aa32cee0b49c082f72df765
Teddy-FN <tedd.ferdy@gmail.com>
2024-04-15 18:27:46 +0700

    Fixing Feature Student Detail

--- controller/student.js
+++ controller/student.js
@@ -3,8 +3,7 @@ const Student = require("../models/student");
 // Index Student List
 exports.renderStundent = (req, res, next) => {
   Student.fetchAllStudent((students) => {
-    console.log("STUDENT =>", students);
-    res.render("user/index.ejs", {
+    res.render("student/index.ejs", {
       pageTitle: "Student",
       path: "/student/student",
       addNewTitle: "student",
@@ -15,7 +14,7 @@ exports.renderStundent = (req, res, next) => {
 
 // Render Form Add New Student
 exports.renderFormAddStudent = (req, res, next) => {
-  res.render("user/add-student.ejs", {
+  res.render("student/add-student.ejs", {
     pageTitle: "Add Student",
     path: "/student/student",
     addNewTitle: "student",
@@ -36,9 +35,9 @@ exports.postUser = (req, res, next) => {
 exports.getDetailUser = (req, res, next) => {
   Student.findById(req.params.id, (user) => {
     // get Detail User
-    res.render("user/detail-user.ejs", {
-      pageTitle: "Detail User",
-      path: "/user/users",
+    res.render("student/detail-student.ejs", {
+      pageTitle: "Detail student",
+      path: "/student/student",
       user: user,
     });
   });
