808881cffe2094ac199b862c3537c68a489c5f99
Teddy-FN <tedd.ferdy@gmail.com>
2024-05-05 12:30:29 +0700

    Setup MongoDB In Project

--- utils/database.js
+++ utils/database.js
@@ -10,10 +10,30 @@
 // module.exports = pool.promise();
 
 // Sequelize
-const Sequelize = require("sequelize"),
-  sequelize = new Sequelize("school-app", "root", "teddyferdian98", {
-    dialect: "mysql",
-    host: "localhost",
-  });
+// const Sequelize = require("sequelize"),
+//   sequelize = new Sequelize("school-app", "root", "teddyferdian98", {
+//     dialect: "mysql",
+//     host: "localhost",
+//   });
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+// Mongo DB
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+// mongodb+srv://ferdiamrullah23:<password>@cluster0.nnen8dj.mongodb.net/
+const mongoClient = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://ferdiamrullah23:teddyferdian98@cluster0.nnen8dj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+    .then((client) => {
+      console.log("Connected");
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = mongoClient;
